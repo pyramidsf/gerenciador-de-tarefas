b92a6a6fa27309b9c310528f92fd2a935f9f1f0c
Maria Eduarda <amorim.mria@gmail.com>
2026-01-29 16:03:52 -0300

    adiciona documentaçao ao readme e aos códigos

--- README.md
+++ README.md
@@ -1,27 +1,36 @@
 # Gerenciador de Tarefas 
 Sistema de gerenciamento de tarefas baseado em microserviços, focado em rápida iteração e hot-reload para desenvolvimento local.
 
+## Tecnologias Utilizadas
+- Conteinerização: Podman e Podman-compose.
+- Proxy/Web Server: Nginx.
+- Backend: Python FastAPI.
+- Banco de Dados: PostgreSQL.
+- Frontend: HTML5 + JavaScript. 
 
 ## Arquitetura do Projeto
-O sistema roda isolado em uma rede interna do Podman.
-1. Cliente (Browser) acessa localhost:8080
+O sistema roda isolado em uma rede interna do **Podman**.
+1. Cliente (Browser) acessa localhost:8080.
 2. **Nginx** serve arquivos estáticos (HTML) no app /.
-3. **Ngnix** redireciona requisições /api/* para o container **Backend**
-4. **Backend** processa a lógica e persiste dados no container **Database**
+3. **Ngnix** redireciona requisições /api/* para o container **Backend**.
+4. **Backend** processa a lógica e persiste dados no container **Database**.
 
 ## Como Rodar 
-
-1. Construa e inicie os containers com o comando: 
+1. Clone esse repositório e acesse o diretório do projeto (**app**). 
+2. Construa e inicie os containers com o comando: 
 ```bash
    podman-compose up --build 
 ```
-
-2. Cheque os containers que estão rodando
+3. Cheque os containers que estão rodando
 ```bash
   podman ps
 ```
-3. Acesse a aplicação:
+4. Acesse a aplicação:
   - Frontend: [http://localhost:8080](http://localhost:8080) 
 
   - Backend API: [http://localhost:8080/api/tarefas/](http://localhost:8080/api/tarefas/)
+5. Para derrubar os containers, utilize o comando: 
+```bash 
+podman-compose down -v
+```
 
--- app/backend/main.py
+++ app/backend/main.py
@@ -1,3 +1,15 @@
+"""
+resumo do fluxo do backend
+1. FastAPI recebe JSON
+2. Verifica se o JSON bate com TabelaCreate 
+3. Chama get.db() para abrir uma sessão 
+4. Roda a função criar()
+5. SQL Alchemy gera o SQL INSERT
+6. Banco devolve o ID
+7. A função retorna o objeto, que é validado pelo TarefaResponse e vira JSON de volta para o navegador. 
+
+"""
+
 from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
@@ -13,17 +25,17 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# modelo db
+# modelo db, onde é criado as tabelas do banco de dados
 class Tarefa(Base):
     __tablename__ = "tarefas"
     id = Column(Integer, primary_key=True, index=True)
     titulo = Column(String, index=True)
     concluida = Column(Boolean, default=False)
 
-# tabelas
+# verifica se a tabela de tarefas existe ou não. cria se não existir 
 Base.metadata.create_all(bind=engine)
 
-# modelo do pydantic 
+# schemas pydantic. o fastapi vai usar essa tecnologia para verificar que os dados entrando e saindo estejam no formato certo 
 class TarefaCreate(BaseModel):
     titulo: str
     concluida: bool = False
@@ -35,7 +47,7 @@ class TarefaResponse(TarefaCreate):
         orm_mode = True 
         from_attributes = True
 
-# dependencia da db 
+# injeçao de dependencia da db. garante que o banco seja abero e fechado no final da requisiçao 
 def get_db():
     db = SessionLocal()
     try:
@@ -45,10 +57,14 @@ def get_db():
 
 app = FastAPI()
 
+# aqui ficam as rotas, a lógica da api
+
+# rota para listar tarefa (GET)
 @app.get("/tarefas/", response_model=list[TarefaResponse])
 def listar(db: Session = Depends(get_db)):
     return db.query(Tarefa).all()
 
+# rota para criar tarefa (POST)
 @app.post("/tarefas/", response_model=TarefaResponse)
 def criar(tarefa: TarefaCreate, db: Session = Depends(get_db)):
     db_item = Tarefa(**tarefa.dict())  
@@ -57,6 +73,7 @@ def criar(tarefa: TarefaCreate, db: Session = Depends(get_db)):
     db.refresh(db_item)
     return db_item
 
+# rota para deletar tarefa (DELETE)
 @app.delete("/tarefas/{id}")
 def deletar(id: int, db: Session = Depends(get_db)):
     item = db.query(Tarefa).filter(Tarefa.id == id).first()  
@@ -66,6 +83,7 @@ def deletar(id: int, db: Session = Depends(get_db)):
     db.commit()
     return {"ok": True}
 
+# rota para atualizar tarefa (PUT)
 @app.put("/tarefas/{id}", response_model=TarefaResponse)
 def atualizar(id: int, tarefa: TarefaCreate, db: Session = Depends(get_db)):
     item = db.query(Tarefa).filter(Tarefa.id == id).first()  
